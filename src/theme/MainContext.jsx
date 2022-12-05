5eb03de7fdb42693f7d22bbdb00f56f0e36ec772
choesuna <sunflower203040@gmail.com>
2022-12-05 10:40:34 +0900

    수정

--- src/theme/MainContext.jsx
+++ src/theme/MainContext.jsx
@@ -1,23 +1,33 @@
 import {useContext} from 'react';
 import ThemeContext from './ThemeContext';
 import Button from '../copied/Button';
+// import styled from 'styled-components'
+
 
 function MainContext(props) { 
     const { theme, toggleTheme } = useContext(ThemeContext);
     
+    // const ThemeToggle = styled.div`
+    // width: 500px;
+    // height: 500px;
+    // padding: 1.5rem;
+    // backgroun-color: ${theme === "light" ? "white" : "black"}
+    // `;
+
+
     return (
-        <div
+        <ThemeToggle
             style={{
                 width: "100vw",
-                height: "50vh",
+                height: "100vh",
                 padding: "1.5rem",
-                backgroundColor: theme === "light" ? "white" : "black",
-                color: theme === "light" ? "black" : "white",
+                backgroundColor: theme == "light" ? "white" : "black",
+                color: theme == "light" ? "black" : "white",
             }}
         >
             <p>테마 변경해 보세요</p>
             <Button onClick={ toggleTheme }>테마 변경</Button>
-        </div>
+        </ThemeToggle>
     )
 }
 
